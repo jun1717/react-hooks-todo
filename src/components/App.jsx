65b37053bd6d03843d2e5cdc897f3cf0169be366
里屋純平 <satoya@satoyajunpei-no-MacBook-Pro.local>
2019-09-07 12:11:48 +0900

    create TodoList with ReactHooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => (
-  <div>
-    Hello, World
-  </div>
-);
+
+const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState('');
+  const addTodo = () => {
+    if (text === '') {
+      return;
+    }
+    setText('');
+    setTodo([...todo, text]);
+  };
+  const deleteTodo = (idx) => {
+    const list = todo.filter((data, index) => index !== idx);
+    setTodo([list]);
+  };
+  return (
+    <div>
+      Todo Sample
+      <div>
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <br />
+        <button type="button" onClick={() => addTodo()}>Add</button>
+        <br />
+        {todo.map((data, index) => (
+          <li key={Math.random()}>
+            <span style={{ paddingRight: '5px' }}>{data}</span>
+            <button type="button" onClick={() => deleteTodo(index)}> delete</button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default App;
